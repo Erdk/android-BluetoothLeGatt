863ffc39356ca0dede40b7c0d673090d6faff08c
Erdk <mr.erdk@gmail.com>
2018-03-28 01:26:20 +0200

    Add GATT services and attributes

--- Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -22,17 +22,30 @@ import java.util.HashMap;
  * This class includes a small subset of standard GATT attributes for demonstration purposes.
  */
 public class SampleGattAttributes {
-    private static HashMap<String, String> attributes = new HashMap();
+    private static HashMap<String, String> attributes = new HashMap<>();
     public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
 
     static {
-        // Sample Services.
-        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        // GATT Services
+        attributes.put("00001802-0000-1000-8000-00805f9b34fb", "Immediate Alert");
+        attributes.put("00001803-0000-1000-8000-00805f9b34fb", "Link Loss");
         attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
-        // Sample Characteristics.
-        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
-        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+
+        // 180a - Device Information
+        attributes.put("00002a23-0000-1000-8000-00805f9b34fb", "System ID");
+        attributes.put("00002a24-0000-1000-8000-00805f9b34fb", "Model Name");
+        attributes.put("00002a25-0000-1000-8000-00805f9b34fb", "Serial Number");
+        attributes.put("00002a26-0000-1000-8000-00805f9b34fb", "Firmware Revision");
+        attributes.put("00002a27-0000-1000-8000-00805f9b34fb", "Hardware Revision");
+        attributes.put("00002a28-0000-1000-8000-00805f9b34fb", "Software Revision");
+        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name");
+        attributes.put("00002a2a-0000-1000-8000-00805f9b34fb", "IEEE 11073-20601 Regulatory Certification Data List");
+        attributes.put("00002a50-0000-1000-8000-00805f9b34fb", "PnP ID");
+
+        // 1802 - Immediate Alert
+        // 1803 - Link Loss
+        attributes.put("00002a06-0000-1000-8000-00805f9b34fb", "Alert Level ");
     }
 
     public static String lookup(String uuid, String defaultName) {
